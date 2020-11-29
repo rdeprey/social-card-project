d9461b8a208f4eabd0f4f610f8a02762bb8e2d70
Rebecca Deprey <rdeprey@gmail.com>
2020-11-29 15:56:41 -0500

    add environment variables to setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 
+// Use dotenv to access environment variables in the app
+require('dotenv').config();
+
 export default function App() {
   const [data, setData] = useState();
 
